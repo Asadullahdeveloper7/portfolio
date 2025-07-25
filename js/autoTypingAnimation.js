905416e90c3f641b8300c96ed72a0bbe7e9adf05
asadullahdeveloper7 <asadullahdeveloper7@gmail.com>
2025-07-25 15:29:21 +0500

    portfolio

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I'm Asad Ullah</span> , a Software Engineering student with a strong passion for technology and problem-solving. I’m currently pursuing my degree while also building real-world experience through hands-on projects and client work.With a growing skill set in full stack web development, I’ve built several personal and academic projects that demonstrate my ability to design, develop, and deploy modern web applications.In addition to software development, I’ve worked for the past 2 years as a <span>Computer Hardware Technician at Rapid Computer</span>, where I’ve gained valuable experience troubleshooting, assembling systems, and providing technical support. This role has sharpened my analytical thinking and deepened my understanding of how hardware and software interact.I'm a quick learner, a team player, and someone who loves turning ideas into reality using code. Whether it's front-end design or back-end logic, I enjoy crafting complete solutions that are both functional and user-friendly."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
